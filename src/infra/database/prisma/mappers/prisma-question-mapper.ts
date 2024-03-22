8287e3269160f381d9f47b6860537063939463f1
balvesD3v <lpbalves7filho@gmail.com>
2024-03-22 11:37:02 -0300

    prisma done for a time

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,21 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { Question as PrismaQuestion } from '@prisma/client'
+
+export class PrismaQuestionMapper {
+  static toDomain(raw: PrismaQuestion): Question {
+    return Question.create(
+      {
+        authorId: new UniqueEntityId(raw.authorId),
+        title: raw.title,
+        content: raw.content,
+        bestAnswerId: undefined,
+        slug: Slug.create(raw.slug),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+}
