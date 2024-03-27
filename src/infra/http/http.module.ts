07e4aeb192c1fa05b1c7e48b5ae4516859e9472d
balvesD3v <lpbalves7filho@gmail.com>
2024-03-27 18:43:27 -0300

    choose question best answer done

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -23,6 +23,8 @@ import { DeleteAnswerController } from './controllers/delete-answer.controller'
 import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer'
 import { FetchQuestionsAnswersController } from './controllers/fetch-questions-answers.controller'
 import { FecthQuestionsAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-questions-answers'
+import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller'
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
 
 @Module({
   imports: [DatabaseModule, CryptograpyModule],
@@ -38,6 +40,7 @@ import { FecthQuestionsAnswersUseCase } from '@/domain/forum/application/use-cas
     EditAnswerController,
     DeleteAnswerController,
     FetchQuestionsAnswersController,
+    ChooseQuestionBestAnswerController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -51,6 +54,7 @@ import { FecthQuestionsAnswersUseCase } from '@/domain/forum/application/use-cas
     EditAnswerUseCase,
     DeleteAnswerUseCase,
     FecthQuestionsAnswersUseCase,
+    ChooseQuestionBestAnswerUseCase,
   ],
 })
 export class HttpModule {}
