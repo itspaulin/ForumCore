7b2acbbd57e5b71e918cf12d636cbaa2c02df3cf
balvesD3v <lpbalves7filho@gmail.com>
2024-04-01 18:48:16 -0300

    ajustes no cache module

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -7,7 +7,6 @@ import { RedisService } from './redis/redis.service'
 @Module({
   imports: [EnvModule],
   providers: [
-    RedisService,
     {
       provide: CacheRepository,
       useClass: RedisCacheRepository,
